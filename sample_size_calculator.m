c36444f0fe3a49202e53e13ea349a9e49568978a
Matto <64538595+mleeuwis@users.noreply.github.com>
2025-02-12 16:21:36 +0100

    Create sample_size_calculator.m

---
+++ sample_size_calculator.m
@@ -0,0 +1,22 @@
+%% Quick sample size estimation
+% Test assumes repeated measures between conditions. We lack sufficient data
+% to estimate a proper standard deviation of participant means, so estimate
+% a standard deviation from the differences observed between current
+% conditions. 
+
+% Intended distinguisable difference from baseline to condition of interest
+difference = 0.1;  %W/kg
+
+% Estimate standard deviation of within-subject difference from baseline to
+% condition of interest
+std_diff = 0.15; %<- manually estimated from previous pilots
+
+% Compute effect size (Cohen's d for paired data)
+effect_size = difference / std_diff;
+
+% Sample size calculation
+alpha = 0.05;    % Significance level
+power = 0.8;     % Desired power
+
+n = sampsizepwr('t', [0, std_diff], difference, power);
+disp(['Required sample size: ', num2str(ceil(n))]);
